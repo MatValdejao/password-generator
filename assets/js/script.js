4fa1b11b3bf0de0ed8ae8f3203197a21ccca5de4
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-06-20 23:04:30 -0400

    added function randomizers to each character type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var theNumbers = "0123456789"
 
 function passwordSpecifications() {
     //prompt user for number of characters and turn variable into type Number
     var length = prompt("How many characters will your password contain?");
-    length = Number(length);
-
     //checks whether length chosen is within 8-128 characters long
-    if (length < 8){
+    if (parseInt(length) < 8 || length === "") {
         alert("Your password is too short! It must be at least 8 characters long!");
         passwordSpecifications();
-    } else if (length > 128) {
+    } else if (parseInt(length) > 128) {
         alert("Your password is too long! It cannot exceed 128 characters!");
         passwordSpecifications();
     }
@@ -18,7 +20,7 @@ function passwordSpecifications() {
     //check what type of characters to use and save into boolean type variables using confirm
     var chooseLowercase = confirm("Would you like to have lowercase characters?");
     var chooseUppercase = confirm("Would you like to use uppercase characters?");
-    var chooseNumeric = confirm("Would you like to use numeric charcaters?");
+    var chooseNumeric = confirm("Would you like to use numeric characters?");
     var chooseSpecial = confirm("Would you like to use special characters?");
 
     //checks whether at least one prompt boolean value is true
@@ -27,25 +29,35 @@ function passwordSpecifications() {
         passwordSpecifications();
     }
 
-    var passwordIdentifiers = {
-        length: length,
-        uppercase: chooseUppercase,
-        lowercase: chooseLowercase,
-        numeric: chooseNumeric,
-        special: chooseSpecial
-    };
+    //returns the chosen length, and users choices of character types as booleans
+    return [length, chooseLowercase, chooseUppercase, chooseNumeric, chooseSpecial];
 }
 
 
+
+//Series of functions that randomly choose one of the characters in each of the corresponding string variables
+function getUppercase() {
+    return uppercaseAlphabet[Math.floor(Math.random()*uppercaseAlphabet.length)];
+}
+
+function getLowercase() {
+    return lowercaseAlphabet[Math.floor(Math.random()*lowercaseAlphabet.length)];
+}
+
+function getNumeric() {
+    return theNumbers[Math.floor(Math.random()*theNumbers.length)];
+}
+
+function getSpecial() {
+    return specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", passwordSpecifications);
